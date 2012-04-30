a553782ef55bb521cb3c1076c319648a6d30cc4c
hvelarde <hector.velarde@gmail.com>
2012-04-30 16:26:57 -0300

    se corrige la declaración del paquete y se agregan clasificadores

--- setup.py
+++ setup.py
@@ -2,31 +2,32 @@ from setuptools import setup, find_packages
 import os
 
 version = '1.0'
-
-long_description = (
-    open('README.rst').read()
-    + '\n' +
-    open('docs/CREDITS.txt').read()
-    + '\n' +
-    open('docs/HISTORY.txt').read()
-    + '\n')
+long_description = open("README.rst").read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
 
 setup(name='telesur.forums',
       version=version,
       description="",
       long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
         "Framework :: Plone :: 4.1",
+        "Intended Audience :: System Administrators",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Topic :: Office/Business :: News/Diary",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
+      keywords='telesur forum',
       author='Franco Pellegrini',
       author_email='frapell@ravvit.net',
-      url='https://github.com/desarrollotv/telesur.forums',
+      url='https://github.com/telesur/telesur.forums',
       license='GPL',
       packages=find_packages('src'),
       package_dir={'': 'src'},
@@ -34,23 +35,18 @@ setup(name='telesur.forums',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-          'plone.app.dexterity>=1.2.1',
-          'plone.principalsource',
-          'collective.prettydate',
-          'borg.localrole',
-          'plone.namedfile[blobs]',
-      ],
+        'setuptools',
+        'borg.localrole',
+        'plone.app.dexterity>=1.2.1',
+        'plone.namedfile[blobs]',
+        'plone.principalsource',
+        'collective.prettydate',
+        ],
       extras_require={
         'test': ['plone.app.testing'],
         },
       entry_points="""
-      # -*- Entry points: -*-
-
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
       )
